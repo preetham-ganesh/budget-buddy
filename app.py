68d8c806b8004b6a23be3b68f6087740e3e15e62
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2023-09-04 17:50:15 -0400

    [#4] added signup function.

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ sys.path.append(BASE_PATH)
 
 
 from flask import Flask, render_template, request, flash, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 import secrets
 import string
 
@@ -38,6 +37,45 @@ app.secret_key = "".join(
     secrets.choice(string.ascii_letters + string.digits + string.punctuation)
     for _ in range(24)
 )
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup() -> str:
+    """Renders template for 'signup.html', and registers user into the database.
+
+    Renders template for 'signup.html', and registers user into the database.
+
+    Args:
+        None.
+
+    Returns:
+        A string for the rendered template for 'signup.html'.
+    """
+    # If request method is 'post', then registers user to the application.
+    if request.method == "POST":
+        email_id = request.form["email"]
+        username = request.form["username"]
+        password = request.form["password"]
+
+        # Checks the validity of the email_id.
+        if not validate_email_id(email_id):
+            flash("- Invalid Email ID.", "error")
+
+        # Checks the validity of the username.
+        if not validate_username(username):
+            flash("- Invalid username. (Use alphanumeric characters)", "error")
+
+        # Check if the email is already registered in the database. If yes, then redirects to login.
+        if User.query.filter_by(email_id=email_id).first():
+            flash("- Email ID is already registered.", "error")
+
+        else:
+            # Create a new user using entered information.
+            new_user = User(email_id=email_id, username=username, password=password)
+            user_db.session.add(new_user)
+            user_db.session.commit()
+            return redirect(url_for("signup"))
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
