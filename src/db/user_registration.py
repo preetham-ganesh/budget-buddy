e6000ffc0c69f45a8e21c0e31e39f15098e6e8ff
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2023-09-04 09:38:35 -0400

    [#4] added validate_email_id function.

--- src/db/user_registration.py
+++ src/db/user_registration.py
@@ -2,6 +2,7 @@
 
 
 from flask_sqlalchemy import SQLAlchemy
+import re
 
 
 # Creates an object for SQAlchemy class.
@@ -16,6 +17,27 @@ class User(db.Model):
     """
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email_id = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(16), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+
+
+def validate_email_id(email_id: str) -> bool:
+    """Checks the validity of the email_id.
+
+    Checks the validity of the email_id.
+
+    Args:
+        email_id: A string for email_id which needs validation.
+
+    Returns:
+        A boolean value for the validity of the email_id, i.e., true if valid, and false if invalid.
+    """
+    # Checks type of arguments.
+    assert isinstance(email_id, str), "Argument email_id should be of type 'str'."
+
+    # Checks if the string text is a valid email.
+    if re.match("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$", email_id):
+        return True
+    else:
+        return False
